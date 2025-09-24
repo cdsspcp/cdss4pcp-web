7f2d00b4c8cf55448f9815f7e7b03016d545002e
ameyadeshp37 <115604036+ameyadeshp37@users.noreply.github.com>
2025-09-24 13:02:47 -0400

    Update QuickLinks.js

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -2,6 +2,38 @@ function $1(props) {
     return (
         <ul>
             <li>
+  <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC12150700/" target="_blank" rel="noopener noreferrer">Reusable Generic Clinical Decision Support System Module for Immunization Recommendations in Resource-Constraint Settings</a>
+  <p>Orlioglu S*, Boobalan AS*, Abanyie K*, Boyce RD, Min H, Gong Y, Sittig DF, Biondich P, Wright A, Nøhr C, Law T, Robinson D, Faxvaag A, Hubig NC, Gimbel R, Rennert L, Jing X</p>
+  <p>AMIA Summit 2025. Mar 10–13, 2025. Pittsburgh, Pennsylvania. eCollection. 395–404. PMID: 40502239; PMCID: PMC12150700</p>
+</li>
+
+<li>
+  <a href="https://doi.org/10.3390/vaccines13050437" target="_blank" rel="noopener noreferrer">Vaccination schedules recommended by the Centers for Disease Control and Prevention: From human-readable to machine-processable</a>
+  <p>Jing X, Min H, Gong Y, Ernst MA*, Weaver A*, Crozier C*, Robinson D, Sittig DF, Biondich P, Orlioglu S*, Boobalan AS*, Abanyie K*, Boyce RD, Wright A, Nøhr C, Law TD, Faxvaag A, Rennert L, Gimbel RW</p>
+  <p>Vaccines. 13(5), 437. PMID: 40432049; PMCID: PMC12116024</p>
+</li>
+
+<li>
+  <a href="https://doi.org/10.36922/jci.4496" target="_blank" rel="noopener noreferrer">Perspectives on Enhancing Clinical Informatics Education in the Artificial Intelligence Era</a>
+  <p>Min H, Jing X, Gong Y, Yu P</p>
+  <p>Journal of Clinical Informatics. JCI 2024, 1(1), 4496. PMID: 40051784; PMCID: PMC11884742</p>
+</li>
+
+<li>
+  <a href="https://doi.org/10.3233/SHTI240660" target="_blank" rel="noopener noreferrer">Active learning pipeline to identify candidate terms for a CDSS ontology</a>
+  <p>Jing X, Goli R*, Komatineni K*, Alluri S*, Hubig N, Min H, Gong Y, Sittig DF, Biondich P, Robinson D, Nøhr C, Faxvaag A, Wright A, Law T, Rennert L, Gimbel R</p>
+  <p>Stud Health Technol Inform. 2024 Aug 22;316:1338–1342. PMID: 39176629; PMCID: PMC12149559</p>
+</li>
+
+<li>
+  <a href="https://doi.org/10.3233/SHTI240638" target="_blank" rel="noopener noreferrer">Challenges and Opportunities of Artificial Intelligence in CDSS and Patient Safety</a>
+  <p>Gong Y, Min H, Jing X, Yu P</p>
+  <p>Stud Health Technol Inform. 2024 Aug 22;316:1250–1254. PMID: 39176608; PMCID: PMC12145886</p>
+</li>
+
+        
+        
+            <li>
             <a href="https://medrxiv.org/cgi/content/short/2025.04.15.25325868v3" target="_blank" rel="noopener noreferrer"> Active learning pipeline to automatically identify candidate terms for a CDSS ontology—measures, experiments, and performance </a>
             <p>Alluri S*, Komatineni K*, Goli R*, Boyce RD, Hubig N, Min H, Gong Y, Sittig DF, Robinson D, Biondich P, Wright A, Nøhr C, Law T, Faxvaag A, Gimbel R, Rennert L, Jing X</p>
             <p>MedRxiv, Under Review</p>
